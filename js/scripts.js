ecec3a2021ec9d801a345ad3c3110cbefe82583b
Grzegorz Koza <kozagrzegorz@interia.pl>
2017-12-17 16:14:47 +0100

    Update js script new content

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var femaleNames = ['Asia', 'Monika', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Kuba', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 var newName = "Marian";
-if(allNames.indexOf(newName) === -1) {
-allNames.push(newName);
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
 }
 console.log(allNames);
+/*
+// reverse specjalnie zakomentowane
 
-// reverse
 var allNames7 = allNames.slice();
 allNames7.reverse();
-console.log(allNames7);
-// Alfabetycznie
+console.log(allNames7);*/
 
-//function compareName(a, b) {
-  //  return a - b
-//}
+//Alfabetycznie
+
 var allNames4 = allNames.slice();
 
-console.log('Bez sortowania: ' + allNames4 );
+console.log('Bez sortowania: ' + allNames4);
 
 allNames4.sort()
-console.log( 'Po sortowaniu: ' + allNames4 );
+console.log('Po sortowaniu: ' + allNames4);
 
 
 // Sortowanie na litere M
@@ -29,29 +28,20 @@ console.log( 'Po sortowaniu: ' + allNames4 );
 var allNames1 = allNames.slice();
 
 console.log(allNames1);
-var filteredByM = allNames1.filter(function(index) {
-				return index !='Marek'&& index != 'Marian';
-		 });
-				console.log('filtered by M:', filteredByM);
 
-
-// sposób 2
-
-console.log(allNames1);
-var filteredByM1 = allNames1.filter(function(el, i) {
-				//console.log(index);
-				return(el.charAt(el.lenght +1) != 'M')
-		 });
-				console.log('filtered by M:', filteredByM1);
+var filteredByM = allNames1.filter(function (el, i) {
+	return (el.charAt(el.i + 1) !== 'M')
+});
+console.log('Filtered by M: ' + filteredByM);
 
 
 // imiona dłuzsze niz 5 liter
 
 var allNames5 = allNames.slice();
-var letter = allNames5.filter(function(el, i) {
-				return (el.charAt(el.length =5))
-		 });
-				console.log('letter', letter);
+var filter = allNames5.filter(function (el) {
+	return (el.length) > 5;
+});
+console.log('letter', filter);
 
 
 // wyciecie pozycji 2-4
@@ -60,14 +50,16 @@ var allNames2 = allNames.slice();
 allNames2.splice(1, 3);
 console.log(allNames2);
 
-
-
 //imiona na K
 
 var allNames6 = allNames.slice();
-var indexOfK = allNames6.filter(function(el, i) {
-				return (el.charAt(el.lenght) =="K")
-		 });
-				console.log('letter K:',  indexOfK);
+var letterK = allNames6.filter(function (el, i) {
+	return (el.charAt(el.i) == "K")
+});
+console.log('letter K:', letterK);
 
 
+
+allNames.map(function (el, i) {
+			console.log(el +' - '+ el.length + ' - letters');
+});
